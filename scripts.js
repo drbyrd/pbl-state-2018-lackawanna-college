d29d8bf3510aa4976dea6c059a95b1331b083909
Justin B <drbyrd@gmail.com>
2018-04-05 01:21:50 +0000

    Fixed youtube playback on modal close, some sources have been inline-commented

--- scripts.js
+++ scripts.js
@@ -1,3 +1,66 @@
+// ############################
+// Stop Youtube video on close
+// Source: https://www.tutorialrepublic.com/faq/how-to-embed-youtube-video-inside-bootstrap-modal.php
+// ############################
+
+$(document).ready(function(){
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+    var url = $("#cartoonVideo").attr('src');
+    
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#myModal").on('hide.bs.modal', function(){
+        $("#cartoonVideo").attr('src', '');
+    });
+    
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed again */
+    $("#myModal").on('show.bs.modal', function(){
+        $("#cartoonVideo").attr('src', url);
+    });
+});
+
+$(document).ready(function(){
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+    var url = $("#cartoonVideo2").attr('src');
+    
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#myModal2").on('hide.bs.modal', function(){
+        $("#cartoonVideo2").attr('src', '');
+    });
+    
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed again */
+    $("#myModal2").on('show.bs.modal', function(){
+        $("#cartoonVideo2").attr('src', url);
+    });
+});
+
+$(document).ready(function(){
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+    var url = $("#cartoonVideo3").attr('src');
+    
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#myModal3").on('hide.bs.modal', function(){
+        $("#cartoonVideo3").attr('src', '');
+    });
+    
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed again */
+    $("#myModal3").on('show.bs.modal', function(){
+        $("#cartoonVideo3").attr('src', url);
+    });
+});
+
+// ############################
+// Scroll to anchor 
+// Source: https://css-tricks.com/snippets/jquery/smooth-scrolling/
+// ############################
 
 $(function() {
 // Select all links with hashes
